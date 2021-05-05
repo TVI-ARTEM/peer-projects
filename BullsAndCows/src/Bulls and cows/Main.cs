f9dff3d8f34b0449e53a98f1b91b3a1f711d07a6
Artem <70791841+TVI-ARTEM@users.noreply.github.com>
2021-05-05 12:52:06 +0600

    Add first project - bulls and cows

---
+++ BullsAndCows/src/Bulls and cows/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace BullsAndCows
+{
+    partial class Program
+    {
+        private static Random rand = new Random();
+
+        private static void Main(string[] args)
+        {
+            do
+            {
+                Title = "Bulls and Cows";
+                Clear();
+                WriteLine("############################################");
+                WriteLine("# Добро пожаловать в игру \"Быки и коровы\"! #");
+                WriteLine("#------------------------------------------#");
+                WriteLine("\nНажмите любую клавишу...");
+                ReadKey();
+                
+                // Вход в главное меню.
+                MainMenu();
+
+                WriteLine("\nДля выхода нажмите ESC, для повтора любую другую клавишу...");
+            } while (ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
